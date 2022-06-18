9ff1c8f3eb45e25d6f8696cf11338645e9fa1f56
Arturo Terrones <arjorete@gmail.com>
2022-06-18 09:22:03 -0600

    agregando las cartas

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,2 +1,16 @@
 
 
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+
+const crearDeck = ()=>{
+
+    for(let i=2; i<=10; i++){
+        for(let tipo of tipos){
+            tipos.push(i + tipo);
+        }
+    }
+    console.log(tipos)
+}
+
+crearDeck();
