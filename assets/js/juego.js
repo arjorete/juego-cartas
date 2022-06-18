f2fad4b29af9f1ea28f552d19ca50202dea4b084
Arturo Terrones <arjorete@gmail.com>
2022-06-18 10:20:15 -0600

    Baraja creada y barajeada

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -2,15 +2,24 @@
 
 let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
 
-const crearDeck = ()=>{
-
-    for(let i=2; i<=10; i++){
-        for(let tipo of tipos){
-            tipos.push(i + tipo);
+//FUNCIONES
+const crearDeck = ()=> {
+    for(let i=2; i <= 10; i++){
+        for(let tipo of tipos) {
+            deck.push(i + tipo); 
         }
     }
-    console.log(tipos)
+
+    for(let esp of especiales){
+        for(let tipo of tipos){
+            deck.push(esp + tipo);
+        }
+    }
+    deck = _.shuffle(deck);
+    console.log(deck);
 }
+
 
 crearDeck();
